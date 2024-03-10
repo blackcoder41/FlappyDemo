2f2175fd582a9d7145f57f41e0b8ac0f3be36627
ral <blackcoder41@gmail.com>
2024-03-10 23:55:29 +0800

    fixed desktop dimension

--- desktop/src/com/brentaureli/game/DesktopLauncher.java
+++ desktop/src/com/brentaureli/game/DesktopLauncher.java
@@ -2,13 +2,14 @@ package com.brentaureli.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.brentaureli.game.FlappyDemo;
+import com.brentaureli.flappybird.FlappyBird;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		config.setWindowedMode(FlappyBird.WIDTH, FlappyBird.HEIGHT);
 		config.setTitle("FlappyDemo");
 		new Lwjgl3Application(new FlappyDemo(), config);
 	}
